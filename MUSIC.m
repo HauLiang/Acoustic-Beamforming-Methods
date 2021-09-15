f946f956c75005111feb58bd1af0bee891f096bd
Hao Liang <haoliang@stu.xmu.edu.cn>
2021-09-15 20:14:04 +0800

    Add files via upload

---
+++ MUSIC.m
@@ -0,0 +1,46 @@
+function MUSIC_result = MUSIC(CSM, hn, nSources)
+%
+% This code implements the MUSIC algorithm
+%
+% More information about MUSIC can be found in the paper:
+%    Schmidt, Ralph, 
+%    "Multiple emitter location and signal parameter estimation", 
+%    IEEE transactions on antennas and propagation, 1986.
+%
+%
+% Inputs:
+%    CSM:  cross-spectrum matrix (CSM)
+%    hn:  steering vector
+%    nSources: number of sources
+%
+% Outputs:
+%    MUSIC_result:  beamforming map, obtained by MUSIC
+%
+% Author: Hao Liang 
+% Last modified by: 21/09/15
+%
+
+% Parameters setting
+[N_X, N_Y, N_mic] = size(hn);
+
+% CSM with diagonal loading
+CSM = CSM + trace(CSM)/(N_mic^2)*eye(N_mic, N_mic);
+CSM = CSM/N_mic;
+
+% Eigenvectors of R
+[Vec, Val] = eig(CSM);
+[~, Seq] = sort(max(Val));
+
+% Noise eigenvectors
+Vn = Vec(:,Seq(1:end-nSources));
+
+% Spatial spectrum (pseudo spectral) imaging
+MUSIC_result = zeros(N_X, N_Y);
+for ii = 1:N_X
+    for jj = 1:N_Y
+        e = reshape(hn(ii,jj,:), N_mic, 1); 
+        MUSIC_result(ii,jj) = 1./(e'*(Vn*Vn')*e);
+    end
+end
+
+end
